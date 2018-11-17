38ddc7d9d9e8f220713d5167d74024d3ce95a3c7
Mokhort <gisellt98@mail.ru>
2018-11-18 02:06:13 +0300

    Lab1. ver 1.1

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -135,6 +135,19 @@ public:
 			}
 	}
 
+	void prodofstore(string storefp) {
+		int count = 0;
+		for (int i = 0; i < tsize; i++)
+			if (mas[i].getStore() == storefp)
+			{
+				cout << mas[i].getProduct() << " " << mas[i].getCost() << endl;
+				count++;
+			}
+		if (count == 0)
+		{
+			cout << "wrong store" << endl;
+		}
+	}
 
 };
 
@@ -167,10 +180,64 @@ int main() {
 	p.show();
 
 	/*any(p);*/
+	int r;
+	bool fl = true;
+	while (fl) {
+		cout << "0 - exit" << endl;
+		cout << "1 - add objects" << endl;
+		cout << "2 - delete object" << endl;
+		cout << "3 - show objects" << endl;
+		cout << "4 - show procucts of one store" << endl;
+
+		cin >> r;
+		switch (r) {
+		case 0:
+			fl = false;
+			break;
+		case 1:
+			cout << "Write the name of store, product and cost :" << endl;
+
+			cin >> wstore >> wproduct >> wcost;
+			q.setStore(wstore);
+			q.setProduct(wproduct);
+			q.setCost(wcost);
+			//p >> q;
+			p.push(q);
 
 
+			p.sort();	//+сортировка по алфавиту
+			break;
+		case 2:
+			cout << "Write the number, which you want to delete :" << endl;
+			cin >> wnumber;
+			try
+			{
+				p << wnumber;
+				//p.pop(wnumber);
+			}
+			catch (int e)
+			{
+				cout << e << " " << "Error" << endl;
+			}
+			catch (...)
+			{
+				cout << "Error" << endl;
+			}
+			break;
+		case 3:
+			p.sort();//сортировка по алфавиту + show
+			p.show();
+			cout << endl;
+			break;
+		case 4:
+			cout << "write a store:" << endl;
+			cin >> storefp;
+			p.prodofstore(storefp);// вывод всех товаров магазина
+			break;
 
-	_getch();
+			_getch();
+		}
+	}
 
 }
 
