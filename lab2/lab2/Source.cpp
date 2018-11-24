7b16805c14f1ee0270423d37d97f283c5c998ed8
Mokhort <gisellt98@mail.ru>
2018-11-24 13:28:34 +0300

    Lab2-Final version

--- lab2/lab2/Source.cpp
+++ lab2/lab2/Source.cpp
@@ -2,49 +2,100 @@
 #include <fstream>
 #include <string>
 #include <conio.h>
-#include <vector>
+
 using namespace std;
+
+class MyVec {
+public:
+	int tsize, n, counter = 1;
+	string *mas;
+	MyVec()
+	{
+		cout << "\n //Constructor//\n";
+		n = 1;
+		tsize = 0;
+		mas = new string[n];
+	}
+
+	~MyVec() {
+		cout << "\n //Destructor//\n";
+		delete[]mas;
+	}
+
+
+	void push(string p)
+	{
+		string *tmp = new string[tsize + 1];
+
+		for (int i = 0; i < tsize; i++)
+			tmp[i] = mas[i];
+		tmp[tsize] = p;
+
+		delete[] mas;
+		mas = tmp;
+		tsize++;
+
+	}
+
+	void show() {
+		if (tsize == 0) {
+			cout << "spisok is empty" << endl;
+		}
+		else {
+			for (int i = 0; i < tsize; i++)
+				cout << mas[i];
+			cout << endl;
+		}
+
+	}
+	void sort() {
+		for (int i = 0; i < tsize; i++)
+		{
+
+			if (mas[i][1] == ' ')
+
+				cout << mas[i] << ' ';
+
+			if ((mas[i][0] == ' ') && (mas[i][2] == ' '))
+				cout << mas[i] << ' ';
+
+		}
+
+		for (int i = 0; i < tsize; i++)
+		{
+			if ((mas[i][0] != ' ') && (mas[i][1] != ' '))
+				cout << mas[i] << ' ';
+
+			if (mas[i][0] == ' '&&mas[i][1] != ' '&&mas[i][2] != ' ')
+				cout << mas[i] << ' ';
+
+		}
+	}
+};
 
 int main() {
 	ifstream file("text.txt");
-	vector <string> tx;
 
 
 	char ch;
 	int counter;
 	string sentense;
 
+	MyVec txt;
 	while (file.get(ch))
 	{
 		sentense = sentense + ch;
 
 		if (ch == '.') {
-			tx.push_back(sentense);
+			txt.push(sentense);
 			sentense = "";
 		}
 	}
-	for (int i = 0; i < tx.size(); i++)
-	{
-		if (tx[i][1] == ' ')
-
-			cout << tx[i] << ' ' << endl;
-
-		if ((tx[i][0] == ' ') && (tx[i][2] == ' '))
-			cout << tx[i] << ' ' << endl;
-
-	}
-
-	for (int i = 0; i < tx.size(); i++)
-	{
-		if ((tx[i][0] != ' ') && (tx[i][1] != ' '))
-			cout << tx[i] << ' ' << endl;
-
-		if (tx[i][0] == ' '&&tx[i][1] != ' '&&tx[i][2] != ' ')
-			cout << tx[i] << ' ' << endl;
-
-	}
-
-
+	
+	cout << "Input file:" << endl;
+	txt.show();
+	cout << "Result:" << endl;
+	txt.sort();
 
 	file.close();
 	_getch();
